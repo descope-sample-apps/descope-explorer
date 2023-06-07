581ff17cc87e6143084377756b65fc6b5ee6a407
Eric Sheen <eric.sprog@gmail.com>
2023-06-07 12:44:26 -0400

    fix darkmode logic

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   const currTheme = queryParameters.get("theme")
   const flow = queryParameters.get("flow") || "sign-up-or-in"
 
-  const [noError, setNoError] = useState(currTheme === "light" || "dark")
+  const [noError, setNoError] = useState(currTheme === "light" || currTheme === "dark")
   const [theme, setTheme] = useState(currTheme || "light")
  
   return (
