f35b639a9cd2ccd789f21c63f9fb0f72c33a9f79
Eric Sheen <eric.sprog@gmail.com>
2023-06-06 21:27:41 -0400

    remove border on shrink

--- src/components/AuthFlow.jsx
+++ src/components/AuthFlow.jsx
@@ -6,81 +6,81 @@ import Replay from '@mui/icons-material/Replay';
 
 
 function AuthFlow({ flow, theme }) {
-    const { isAuthenticated } = useSession()
-    const { logout } = useDescope()
-    const [jwt, setJwt] = useState("")
-    const [response, setResponse] = useState("")
+  const { isAuthenticated } = useSession()
+  const { logout } = useDescope()
+  const [jwt, setJwt] = useState("")
+  const [response, setResponse] = useState("")
 
-    useEffect(() => {
-      if (!jwt) {
-        logoutUser() 
+  useEffect(() => {
+    if (!jwt) {
+      logoutUser() 
+    }
+  }, [jwt]) 
+
+  const logoutUser = async() => {
+    await logout()
+  }
+
+  const setJWTs = (e) => {
+    const response = JSON.stringify(e.detail, null, 2)
+    const jwt = decodeJWT(e.detail.sessionJwt)
+
+    setResponse(response)
+    setJwt(jwt)
+  }
+
+  const decodeJWT = (res) => {
+    var tokens = res.split(".");
+    return JSON.stringify(JSON.parse(atob(tokens[1])), null, 2)
+  }
+
+  return (
+    <div className='page authflow'>
+      {isAuthenticated &&  
+        <button className='logout-btn' onClick={logout}><Replay /></button>
       }
-    }, [jwt]) 
-  
-    const logoutUser = async() => {
-      await logout()
-    }
-
-    const setJWTs = (e) => {
-      const response = JSON.stringify(e.detail, null, 2)
-      const jwt = decodeJWT(e.detail.sessionJwt)
-  
-      setResponse(response)
-      setJwt(jwt)
-    }
-  
-    const decodeJWT = (res) => {
-      var tokens = res.split(".");
-      return JSON.stringify(JSON.parse(atob(tokens[1])), null, 2)
-    }
-  
-    return (
-      <div className='page authflow'>
-        {isAuthenticated &&  
-          <button className='logout-btn' onClick={logout}><Replay /></button>
-        }
-        
-        { isAuthenticated &&
-          (
-            <>
-              {jwt && (
-                <div className='row jwt-wrapper'>
-                  <div className='jwt-box'>
-                    <h3 className='jwt-title'>Payload</h3>
-                    <pre className='pre1'>{jwt}</pre>
-                  </div>
-                  <div className='jwt-box'>
-                    <h3 className='jwt-title'>JWT Response</h3>
-                    <pre className='pre2'>{response}</pre>
-                  </div>
+      
+      { isAuthenticated &&
+        (
+          <>
+            {jwt && (
+              <div className='row jwt-wrapper'>
+                <div className='jwt-box'>
+                  <h3 className='jwt-title'>Payload</h3>
+                  <pre className='pre1'>{jwt}</pre>
                 </div>
-              )}  
-            </>
-          )
-        }
+                <div className='jwt-box'>
+                  <h3 className='jwt-title'>JWT Response</h3>
+                  <pre className='pre2'>{response}</pre>
+                </div>
+              </div>
+            )}  
+          </>
+        )
+      }
 
-        { !isAuthenticated && (
-            <div className='flow-shown'>
-              {theme === "light" ?
-                <Descope
-                  flowId={flow} 
-                  onSuccess = {(e) => setJWTs(e)}
-                  onError={(e) => console.log('Could not log in!')}
-                  theme="light"
-                /> 
-                :
-                <Descope
-                  flowId={flow} 
-                  onSuccess = {(e) => setJWTs(e)}
-                  onError={(e) => console.log('Could not log in!')}
-                  theme="dark"
-                /> 
-              }
-            </div>
-          )
-        }
-      </div>
-    )
+      { !isAuthenticated && (
+          <div className='flow-shown'>
+            {theme === "light" ?
+              <Descope
+                flowId={flow} 
+                onSuccess = {(e) => setJWTs(e)}
+                onError={(e) => console.log('Could not log in!')}
+                theme="light"
+              /> 
+              :
+              <Descope
+                flowId={flow} 
+                onSuccess = {(e) => setJWTs(e)}
+                onError={(e) => console.log('Could not log in!')}
+                theme="dark"
+              /> 
+            }
+          </div>
+        )
+      }
+    </div>
+  )
 }
 
 
