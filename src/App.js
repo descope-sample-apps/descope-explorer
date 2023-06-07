525e508a7140935dc3aa90b452618ce9087fa423
Eric Sheen <eric.sprog@gmail.com>
2023-06-07 09:24:19 -0400

    explorerv8

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { AuthProvider } from '@descope/react-sdk'
 import Error from './components/Error';
 import AuthFlow from './components/AuthFlow';
@@ -11,9 +11,23 @@ import SDKShow from './components/SDKs';
 
 function App() {
   const queryParameters = new URLSearchParams(window.location.search)
-  const project = queryParameters.get("project") || "P2QZZJhnbALQo5FSNKRi0KAHHRz6"
+
   const flow = queryParameters.get("flow") || "sign-up-or-in"
   const [theme, setTheme] = useState(queryParameters.get("theme") || "light")
+
+  const defaultProjectId = "P2QZZJhnbALQo5FSNKRi0KAHHRz6"
+  const project_param = queryParameters.get("project")
+  const project = project_param || queryParameters.get("default-project")
+  const url = new URL(window.location.href)
+
+  if(!project_param) {
+    url.searchParams.set('default-project', defaultProjectId)
+  } else {
+    url.searchParams.delete('default-project')
+  }
+
+  const search_params = url.searchParams.toString();
+  window.history.pushState(search_params, "Explorer", url.toString()); // adds an entry to the browser's session history stack.
 
   return (
     <>
